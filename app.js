4de098cc288f23f28495f18d2e138bb2adfc9435
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-14 22:08:31 +0000

    Exemplo

---
+++ app.js
@@ -0,0 +1,6 @@
+// Importando as funcionalidades e constante do arquivo biblioteca.js
+import { somar, multiplicar, pi } from './biblioteca.js';
+
+resp.innerHTML = 'A soma de 3 e 4 é: ', somar(3, 4); // Resultado: 7
+resp.innerHTML = 'A multiplicação de 3 e 4 é: ', multiplicar(3, 4); // Resultado: 12
+resp.innerHTML = 'O valor de pi é: ', pi; // Resultado: 3.14159
---
+++ biblioteca.js
@@ -0,0 +1,12 @@
+// Exportando uma constante:
+export const pi = 3.14159; 
+
+// Exportando funcionalidades:
+export function somar(a, b) {
+    return a + b;
+}
+
+export function multiplicar(a, b) {
+    return a * b;
+}
+
